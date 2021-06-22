005a28bf463578ffb9fdb5166d60a41905a0a7a0
LeaCrazyBambi <lheynitz@gmail.com>
2021-06-22 13:34:31 +0200

    Done

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+import { hoverHeader } from "./hover.js";
+import { clickImages } from "./click.js";
+import { scrollWindow } from "./scroll.js";
+
+console.log(hoverHeader, clickImages, scrollWindow);
